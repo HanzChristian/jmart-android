48ba670c19698e83b05ea8e58cd228adb4eeffcc
HanzChristian <hanz.christian@ui.ac.id>
2021-12-01 13:04:19 +0700

    Pembuatan fragment,menu_main & pengimplementasian pada backend - Hanz Christian

--- app/src/main/java/com/HanzChristianJmartMH/MainActivity.java
+++ app/src/main/java/com/HanzChristianJmartMH/MainActivity.java
@@ -1,13 +1,20 @@
 package com.HanzChristianJmartMH;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.HanzChristianJmartMH.model.Account;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mTopToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.title);
         Account account = LoginActivity.getLoggedAccount();
-        tv.setText("Hello, " + account.name + " !");
+        mTopToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mTopToolbar);
+
+//        tv.setText("Hello, " + account.name + " !");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.search) {
+            Toast.makeText(MainActivity.this, "Search Clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if ( id == R.id.addbox){
+            Toast.makeText(MainActivity.this, "Add Box Clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if (id == R.id.person){
+            Toast.makeText(MainActivity.this, "Person Clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
